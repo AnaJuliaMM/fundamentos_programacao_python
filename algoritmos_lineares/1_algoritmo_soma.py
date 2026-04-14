ff0c43c4a7d5080eb7ecb09471861b77fc2a7a45
Ana Julia Moura Martins <ana.martins_2004@outlook.com>
2026-04-14 19:37:12 -0300

    feat: adicionar enunciado e um exemplo de codigo

---
+++ algoritmos_lineares/1_algoritmo_soma.py
@@ -0,0 +1,12 @@
+# PROGRAMA:
+# Esse programa recebe dois números reais e mostra o total
+
+# ENTRADAS
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# PROCESSAMENTO
+total = numero1 + numero2
+
+# SAÍDA
+print(f"O resultado é {total}")
